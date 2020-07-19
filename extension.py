28bee532a854cd4c62ac6b208b00bb78aab213a4
shaileshchoudhary <43171769+shaileshchoudhary@users.noreply.github.com>
2020-07-19 22:11:49 +0530

    Update extension.py

--- extension.py
+++ extension.py
@@ -3,6 +3,6 @@ for root,dirs,files in os.walk('/root/devops_task3'):
     print(files)
     for file in files:
         if file.endswith('.html'):
-           os.system("kubectl create -f /root/devops_task3/service.yml")
-           os.system("kubectl create -f /root/devops_task3/jenkins_pvc.yml")
-           os.system("kubectl create -f /root/devops_task3/deploy.yml")
+           os.system("sudo kubectl create -f /root/devops_task3/service.yml")
+           os.system("sudo kubectl create -f /root/devops_task3/jenkins_pvc.yml")
+           os.system("sudo kubectl create -f /root/devops_task3/deploy.yml")
